3bf12a0ec0f334dba167b0b1407f9e0b592bf371
Gavin-Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-16 09:32:53 +0300

    add

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,3 @@
 DATABASE_URL = 'postgresql+psycopg2://esagavin:moringa@localhost/blog'
 SECRET_KEY = 'choomba'
 MAIL_USERNAME = 'gavin.kariuki@student.moringaschool.com'
-MAIL_PASSWORD = 'Mzabibu21'
